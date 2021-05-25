e7ec7d279218fbe2bb079ff0dd3d0fecd9e54df2
Michael Valdes <michael.j.valdes@gmail.com>
2021-05-25 18:06:43 -0400

    Implement RecordNormalizer and fix test

--- src/main/java/org/company/RecordNormalizer.java
+++ src/main/java/org/company/RecordNormalizer.java
@@ -1,8 +1,47 @@
 package org.company;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
 public class RecordNormalizer {
 
     public NormalizedRecord transformRecord(Record record) {
-        return new NormalizedRecordBuilder().build();
+        return new NormalizedRecordBuilder()
+                .setTimestamp(changeTimeZoneFromPacificToEastern(record.getTimestamp()))
+                .setZip(padZipToFiveDigits(record.getZip()))
+                .setFullName(convertFullNameToUppercase(record.getFullName()))
+                .setAddress(record.getAddress())
+                .setFooDuration(convertDurationToSeconds(record.getFooDuration()))
+                .setBarDuration(convertDurationToSeconds(record.getBarDuration()))
+                .setTotalDuration(calculateTotalDuration(record))
+                .setNotes(record.getNotes())
+                .build();
+    }
+
+    private LocalDateTime changeTimeZoneFromPacificToEastern(LocalDateTime timeStamp) {
+        ZoneId oldZone = ZoneId.of("America/Los_Angeles");
+        ZoneId newZone = ZoneId.of("America/New_York");
+        return timeStamp.atZone(oldZone)
+                .withZoneSameInstant(newZone)
+                .toLocalDateTime();
+    }
+
+    private String padZipToFiveDigits(String zip) {
+        return StringUtils.leftPad(zip, 5, '0');
+    }
+
+    private String convertFullNameToUppercase(String fullName) {
+        return fullName.toUpperCase();
+    }
+
+    private double convertDurationToSeconds(Duration duration) {
+        return duration.toMillis() / 1000D;
+    }
+
+    private double calculateTotalDuration(Record record) {
+        return convertDurationToSeconds(record.getFooDuration()) + convertDurationToSeconds(record.getBarDuration());
     }
 }
--- src/test/java/org/company/RecordNormalizerTest.java
+++ src/test/java/org/company/RecordNormalizerTest.java
@@ -6,25 +6,24 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.Month;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RecordNormalizerTest {
 
     @Test
     void transformRecord() {
-        Record record =buildRecord();
-
+        Record record = buildRecord();
         NormalizedRecord expectedNormalizedRecord = buildNormalizedRecord();
-        NormalizedRecord normalizedRecord =  new RecordNormalizer().transformRecord(record);
+        NormalizedRecord normalizedRecord = new RecordNormalizer().transformRecord(record);
         assertEquals(expectedNormalizedRecord, normalizedRecord);
     }
 
     private Record buildRecord() {
         Record record = new Record();
-        LocalDateTime dateTime =  LocalDateTime.of(2011, Month.APRIL, 1, 11, 0, 0);
+        LocalDateTime dateTime = LocalDateTime.of(2011, Month.APRIL, 1, 11, 0, 0);
         record.setTimestamp(dateTime);
         record.setZip("1003");
-        record.setFullName("Great name for person");
+        record.setFullName("Great name for a person");
         record.setAddress("Super cool address");
         record.setFooDuration(Duration.ofHours(2));
         record.setBarDuration(Duration.ofMillis(100));
@@ -35,9 +34,9 @@ class RecordNormalizerTest {
 
     private NormalizedRecord buildNormalizedRecord() {
         return new NormalizedRecordBuilder()
-                .setTimestamp( LocalDateTime.of(2011, Month.APRIL, 1, 14, 0, 0))
+                .setTimestamp(LocalDateTime.of(2011, Month.APRIL, 1, 14, 0, 0))
                 .setZip("01003")
-                .setFullName("Great name for person")
+                .setFullName("GREAT NAME FOR A PERSON")
                 .setAddress("Super cool address")
                 .setFooDuration(7200D)
                 .setBarDuration(0.1D)
