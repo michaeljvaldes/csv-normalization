3b38270fcf5acafdc29e4e5c67f17b3dbeaaf542
Michael Valdes <michael.j.valdes@gmail.com>
2021-05-25 14:59:29 -0400

    Implement simple csv reader and split tests

--- src/main/java/org/company/CsvReader.java
+++ src/main/java/org/company/CsvReader.java
@@ -1,12 +1,38 @@
 package org.company;
 
-import java.io.InputStream;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+
+import static com.opencsv.ICSVWriter.DEFAULT_QUOTE_CHARACTER;
 
 public class CsvReader {
 
+    private static final Logger LOGGER = LogManager.getLogger(CsvReader.class);
+
     public List<Record> parseCsvFromInputStream(InputStream inputStream) {
-        return new ArrayList<>();
+        List<Record> records = new ArrayList<>();
+        try (
+                Reader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+        ) {
+            CsvToBean<Record> csvToBean = new CsvToBeanBuilder<Record>(reader)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .withQuoteChar(DEFAULT_QUOTE_CHARACTER)
+                    .withType(Record.class)
+                    .build();
+            Iterator<Record> recordIterator = csvToBean.iterator();
+            recordIterator.forEachRemaining(records::add);
+        } catch (IOException e) {
+            LOGGER.error("Error reading csv to beans; " + e.getMessage());
+        }
+        return records;
     }
 }
+
--- src/test/java/org/company/CsvReaderTest.java
+++ src/test/java/org/company/CsvReaderTest.java
@@ -1,5 +1,6 @@
 package org.company;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
@@ -15,15 +16,28 @@ class CsvReaderTest {
 
     @Test
     public void testSample() {
-        List<Record> records = reader.parseCsvFromInputStream(getClass().getResourceAsStream(SAMPLE_FILE));
+        List<Record> records = getRecordsFromResource(SAMPLE_FILE);
         assertEquals(9, records.size());
         Record monkeyAlbertoRecord = records.stream()
                 .filter(record -> "Monkey Alberto".equals(record.getFullName()))
                 .findAny()
                 .get();
         assertEquals("I am the very model of a modern major general", monkeyAlbertoRecord.getNotes());
+    }
 
+    @Test
+    public void testSampleDateTimeConversion() {
+        List<Record> records = getRecordsFromResource(SAMPLE_FILE);
+        Record monkeyAlbertoRecord = records.stream()
+                .filter(record -> "Monkey Alberto".equals(record.getFullName()))
+                .findAny()
+                .get();
         LocalDateTime expectedDateTime = LocalDateTime.of(2011, Month.APRIL, 1, 11, 0, 0);
         assertEquals(expectedDateTime, monkeyAlbertoRecord.getTimestamp());
     }
+
+    private List<Record> getRecordsFromResource(String resourceName) {
+        return reader.parseCsvFromInputStream(getClass().getClassLoader().getResourceAsStream(resourceName));
+    }
+
 }
