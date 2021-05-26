4e40482bd204f9ab6c444a7879c7108d59f13564
Michael Valdes <michael.j.valdes@gmail.com>
2021-05-25 20:11:25 -0400

    Implement default CsvWriter

--- src/main/java/org/company/CsvWriter.java
+++ src/main/java/org/company/CsvWriter.java
@@ -1,11 +1,30 @@
 package org.company;
 
-import java.io.OutputStream;
+import com.opencsv.bean.StatefulBeanToCsv;
+import com.opencsv.bean.StatefulBeanToCsvBuilder;
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.*;
 import java.util.List;
+
+import static com.opencsv.ICSVWriter.DEFAULT_QUOTE_CHARACTER;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class CsvWriter {
 
-    public void writeCsvToOutputStream(List<NormalizedRecord> records, OutputStream outputStream) {
+    private static final Logger LOGGER = LogManager.getLogger(CsvWriter.class);
 
+    public void writeCsvToOutputStream(List<NormalizedRecord> records, OutputStream outputStream) {
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(outputStream, UTF_8))) {
+            StatefulBeanToCsv<NormalizedRecord> beanToCsv = new StatefulBeanToCsvBuilder<NormalizedRecord>(writer)
+                    .withQuotechar(DEFAULT_QUOTE_CHARACTER)
+                    .build();
+            beanToCsv.write(records);
+        } catch (IOException | CsvDataTypeMismatchException | CsvRequiredFieldEmptyException e) {
+            LOGGER.error("Error writing beans to csv; " + e.getMessage());
+        }
     }
 }
